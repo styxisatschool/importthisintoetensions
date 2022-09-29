0ba4984f96f8d4e75d322109b13ee6f20b78efaf
styxisatschool <114007427+styxisatschool@users.noreply.github.com>
2022-09-29 10:53:11 -0600

    Update import.js

--- import.js
+++ import.js
@@ -1,1 +1,1 @@
-Import("https://raw.githubusercontent.com/styxisatschool/importthisintoetensions/main/thisimport.js")
+Import("raw.githubusercontent.com/styxisatschool/importthisintoetensions/main/thisimport.js")
